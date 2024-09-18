3a54f54b014d372989fabda6e4686bd55f39dd84
Gyuri <haha0888@naver.com>
2024-09-18 15:51:16 +0900

    food 개발중

---
+++ src/main/java/com/knu/salmon/api/domain/food/dto/request/CreateFoodDto.java
@@ -0,0 +1,23 @@
+package com.knu.salmon.api.domain.food.dto.request;
+
+
+import com.knu.salmon.api.domain.food.entity.FoodCategory;
+import jakarta.persistence.Column;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class CreateFoodDto {
+    private String name;
+
+    private Long stock;
+
+    private LocalDateTime expiration;
+
+    private Long price;
+
+    private String content;
+
+    private FoodCategory foodCategory;
+}
---
+++ src/main/java/com/knu/salmon/api/domain/food/entity/Food.java
@@ -0,0 +1,52 @@
+package com.knu.salmon.api.domain.food.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity(name = "foods")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Food {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "food_id")
+    private Long id;
+
+    @Column(name = "food_name")
+    private String name;
+
+    @Column(name = "food_stock")
+    private Long stock;
+
+    @Column(name = "food_expiration")
+    private LocalDateTime expiration;
+
+    @Column(name = "food_price")
+    private Long price;
+
+    @Column(name = "food_content")
+    private String content;
+
+    @Column(name = "food_category")
+    private FoodCategory foodCategory;
+
+    @Builder
+    public Food(String name, Long stock, LocalDateTime expiration, Long price, String content, FoodCategory foodCategory){
+        this.name = name;
+        this.stock = stock;
+        this.expiration = expiration;
+        this.price = price;
+        this.content = content;
+        this.foodCategory = foodCategory;
+    }
+
+
+
+}
