1f2c642a988f50380917cce1c84fe46a6b935f5d
이민혁 <helloaway213@gmail.com>
2024-09-18 14:39:43 +0900

    Init : 초기 환경설정 설정

---
+++ src/test/java/com/knu/salmon/SalmonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knu.salmon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalmonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
